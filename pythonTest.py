3e5b50ab41f275103031d825fa4da28159a8f141
Smaugizard <nicholas.mcfadden@gmail.com>
2020-02-25 22:38:51 -0600

    Add files via upload

---
+++ pythonTest.py
@@ -0,0 +1,11 @@
+test = 0
+
+def detection():
+    haveString = 0
+    haveString = int(input("How much did you find?"))
+    if haveString >= 1:
+        print("You have " + haveString + " String")
+    else
+        print("You dont have enough String...")
+
+detection()
